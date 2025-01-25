a06eebaf9e10f94aaff77a938eed6485fceacad3
Vitor <sblvitor@gmail.com>
2025-01-24 21:19:07 -0400

    create fastify plugin and new transaction route

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,11 @@
 import fastify from "fastify";
-import { knex } from "./database";
-import { randomUUID } from "crypto";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    // .insert({
-    //     id: randomUUID(),
-    //     title: 'Transação de teste',
-    //     amount: 1000
-    // }).returning('*')
-    const transactions = await knex('transactions')
-    .where('amount', 100)
-    .select('*')
-
-    return transactions
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
 })
 
 app.listen({
