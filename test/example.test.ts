21a21486c2123655d8d925d944f01c280c7014c5
Vitor <sblvitor@gmail.com>
2025-01-25 01:41:46 -0400

    add new transaction tests

--- test/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('o usuario consegue criar uma nova transação', () => {
-    const responseStatusCode = 201
-
-    expect(responseStatusCode).toEqual(201)
-})
---
+++ test/transactions.test.ts
@@ -0,0 +1,49 @@
+import {  test, beforeAll, afterAll, describe, it, expect } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    
+    afterAll(async () => {
+        await app.close()
+    })
+    
+    test('user can create a new transaction', async () => {
+         await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+            .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+        
+        const cookies = createTransactionResponse.get('Set-Cookie');
+        console.log(cookies);
+        const listTransactionsResponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies ?? [])
+            .expect(200);
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000
+            })
+        ])
+    })
+})
+
